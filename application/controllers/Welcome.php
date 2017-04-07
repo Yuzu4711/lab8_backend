acdb91291d6dfe47fb8e38f3052f79ad73999bbf
Daniel Tian <daniel_tian2002@hotmail.com>
2017-04-06 17:04:18 -0700

    added restful controller and packages for menu

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,19 +6,10 @@ class Welcome extends Application {
 
 	public function index()
 	{
-		$result = '';
-		$oddrow = true;
-		foreach ($this->categories->all() as $category)
-		{
-//			$viewparms = array(
-//				'direction' => ($oddrow ? 'left' : 'right')
-//			);
-//			$viewparms = array_merge($viewparms, $category);
-			$category->direction = ($oddrow ? 'left' : 'right');
-			$result .= $this->parser->parse('category-home', $category, true);
-			$oddrow = ! $oddrow;
-		}
-		$this->data['content'] = $result;
+
+		//$this->data['title'] = 'Jim\'s Joint';
+		$this->data['pagebody'] = 'welcome_msg';
+
 		$this->render();
 	}
 
